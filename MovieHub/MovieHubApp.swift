761a799850ba0912d166e1bcd6fc45d263666369
Oleksandra <qovtunova.s@gmail.com>
2025-08-30 19:40:24 +0200

    Initial Commit

---
+++ MovieHub/MovieHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieHubApp.swift
+//  MovieHub
+//
+//  Created by Oleksandra Kovtunova on 30/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
